a3a6868ee9dcd19ccc9b22d4298b5917d595a347
m.serra <M.Serra@nier.it>
2026-06-03 14:49:08 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
 from __future__ import annotations
-from typing import (NoReturn)
 import sys
 
+from typing import (NoReturn)
+from sys import (stdout, stderr)
 
 def main() -> NoReturn:
-    sys.stdout.write("$ ")
+    stdout.write("$ ")
+
+    line : str = input()
+    
+    stderr.write("%s: command not found" % line)
     pass
 
 
