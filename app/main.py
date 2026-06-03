ea5368162c7ba48462f603d993f7ed837847b78d
m.serra <M.Serra@nier.it>
2026-06-03 14:54:03 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,14 +4,21 @@ import sys
 from typing import (NoReturn)
 from sys import (stdout, stderr)
 
-def main() -> NoReturn:
+def body() -> int:
     stdout.write("$ ")
 
-    line : str = input()
+    try:
+        line : str = input()
+    except EOFError as _:
+        return -1
     
-    stderr.write("%s: command not found" % line)
-    pass
+    stderr.write("%s: command not found\n" % line)
+    return 0
 
+def main() -> NoReturn:
+    while (0 == body()):
+        ...
+    pass
 
 if __name__ == "__main__":
     main()
